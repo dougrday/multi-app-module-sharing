4ec0abbac387e2c04bdd35c9a0d32b358acb3712
Doug Day <dday@mastercontrol.com>
2025-03-26 14:35:21 -0600

    Fix route issues, fix missing css

--- apps/host/web/src/HostRoutes.tsx
+++ apps/host/web/src/HostRoutes.tsx
@@ -1,14 +1,13 @@
-// FIXME: import these from a targeted import: @example/app1-web/routes and ensure nothing extra is imported
-import App1, { AppRoutes as App1Routes } from "@example/app1-web";
-import { AppRoutes as App2Routes } from "@example/app2-web";
-import { Routes, Route, BrowserRouter } from "react-router";
+import App1 from "@example/app1-web";
+import App2 from "@example/app2-web";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const HostRoutes = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App1 />} />
-            <Route path="/app1/*" element={<App1Routes />} />
-            <Route path="/app2/*" element={<App2Routes />} />
+            <Route path="/app1" element={<App1 />} />
+            <Route path="/app2" element={<App2 />} />
         </Routes>
     </BrowserRouter>
 );
